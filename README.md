8bd33bb02b2370762e6d4a19915c9741013dd2d0
Nickolai Lychagin <belarusconsul@gmail.com>
2022-02-17 15:25:06 +0300

    Add docstrings to flaskapp/main.py, add requirements.txt, update README.md

---
+++ README.md
@@ -0,0 +1,35 @@
+# Flask app
+
+
+### Project for Python L2 Mentoring Program \#2
+
+
+### Author: Nickolai Lychagin, Nickolai_Lychagin@epam.com
+### Version 0.1, 17.02.2022
+
+> This program:
+>
+> - returns "Health check done. Status: OK" on http://localhost:80
+
+---
+## USAGE
+
+**Windows:**<br>
+`python flaskapp.py` (run from directory with flaskapp.py) or<br>
+`python -m flaskapp` (if directory with flaskapp.py is in sys.path)<br>
+**Unix:**<br>
+`python3 flaskapp.py`(run from directory with flaskapp.py) or<br>
+`python3 -m flaskapp`(if directory with flaskapp.py is in sys.path)<br>
+
+---
+## FOLDER STRUCTURE
+
+### flaskapp - package for Flask app:
+#### \_\_init\_\_.py - package initialization file
+#### \_\_main\_\_.py - allow program to be run by package name
+#### main.py - main program logic
+
+### .gitignore - ignore files for GIT
+### flaskapp.py - run program from command line
+### README.md - information about this program
+### requirements.txt - required python packages
--- flaskapp/main.py
+++ flaskapp/main.py
@@ -1,3 +1,55 @@
+#!/usr/bin/env python3
+
+"""Flask app.
+
+This program:
+
+    - returns "Health check done. Status: OK" on http://localhost:80
+
+Usage:
+
+    Windows:
+
+        python flaskapp.py
+        (run from directory with flaskapp.py)
+
+        python -m flaskapp
+        (if directory with flaskapp.py is in sys.path)
+
+    Unix:
+
+        python3 flaskapp.py
+        (run from directory with flaskapp.py)
+
+        python3 -m flaskapp
+        (if directory with flaskapp.py is in sys.path)
+
+Package structure:
+
+    flaskapp - package for Flask app:
+
+        __init__.py
+        package initialization file
+
+        __main__.py
+        allow program to be run by package name
+
+        main.py
+        main program logic
+
+    .gitignore
+    ignore files for GIT
+
+    flaskapp.py
+    run program from command line
+
+    README.md
+    information about this program
+
+    requirements.txt
+    required python packages
+"""
+
 from flask import Flask
 
 HEALTH_STATUS = True
@@ -8,6 +60,7 @@ app.debug = False
 
 @app.route('/')
 def index():
+    """Route for homepage. Return information about health status"""
     message = "Health check done. Status: "
     if HEALTH_STATUS:
         return f"<h1>{message} OK</h1>"
